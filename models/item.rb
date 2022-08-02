164495c8bce608eec318f071e4bf0550574a0a30
Lembani Sakala <lembanisakala@gmail.com>
2022-08-02 17:43:29 +0200

    Create Item class

--- models/item.rb
+++ models/item.rb
@@ -1,17 +1,19 @@
 class Item
-  def initialize(genre, author, source, label, publish)
-    @genre = genre
-    @author = author
-    @source  = source
-    @label = label
+  attr_accessor :genre, :author, :source, :label, :publish_date, :archived, :id
+
+  def initialize(id, publish_date)
+    @id = id
+    @archived = false
+    @publish_date = publish_date
   end
 
   def can_be_archived?
-    @archived
+    @publish_date > 10
   end
 
   private :can_be_archived
 
   def move_to_archive
+    @archived = true if can_be_archived?
   end
 end
