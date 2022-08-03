857a0328434f8646e2aed8ab9ad7948f8e970871
Lembani Sakala <lembanisakala@gmail.com>
2022-08-03 12:06:17 +0200

    Fix Association

--- models/genre.rb
+++ models/genre.rb
@@ -4,9 +4,12 @@ class Genre
   attr_reader :id
   attr_accessor :name, :items
 
-  def initialize(name)
+  def initialize(name, item)
     @id = Random.rand(1...1000)
     @name = name
+    @item = item
+    @iten.genre << self
+
     @items = []
   end
 
