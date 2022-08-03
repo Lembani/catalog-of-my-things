09f6a45df0521d842bf94b1cfe5b4615426b3618
Lembani Sakala <lembanisakala@gmail.com>
2022-08-03 10:31:24 +0200

    Create Genre class

---
+++ models/genre.rb
@@ -0,0 +1,12 @@
+require './item'
+
+class Genre
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @items = []
+  end
+end
