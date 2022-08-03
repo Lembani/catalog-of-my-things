df04575c0ba353cd400189d25dc48f76ca127a4b
Lembani Sakala <lembanisakala@gmail.com>
2022-08-03 13:10:06 +0200

    Add genre

--- models/genre.rb
+++ models/genre.rb
@@ -2,19 +2,16 @@ require './item'
 
 class Genre
   attr_reader :id
-  attr_accessor :name, :items
+  attr_accessor :name, :item
 
-  def initialize(name, item)
+  def initialize(name)
     @id = Random.rand(1...1000)
     @name = name
-    @item = item
-    @iten.genre << self
-
     @items = []
   end
 
   def add_item(item)
-    @items << item
-    item.genre = self
+    item.add_genre = self
+    @items.push(item)
   end
 end
