d77039ca758314c2ec3d6706165aee3b4f854205
Lembani Sakala <lembanisakala@gmail.com>
2022-08-03 20:03:25 +0200

    Update Genre

--- models/genre.rb
+++ models/genre.rb
@@ -1,20 +1,17 @@
-require './item'
+require_relative './item'
 
 class Genre
   attr_reader :id
   attr_accessor :name, :item
 
-  def initialize(name, item)
+  def initialize(name)
     @id = Random.rand(1...1000)
     @name = name
-    @item = item
-    @iten.genre << self
-
     @items = []
   end
 
   def add_item(item)
-    item.add_genre = self
-    @items.push(item)
+    item.genre = self
+    @items << item
   end
 end
