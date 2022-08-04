0f27fbcd1f4a7d8892b9fb46a7c5334e8a1d19eb
Lembani Sakala <lembanisakala@gmail.com>
2022-08-04 17:52:03 +0200

    Update Item controller

--- controllers/item_controller.rb
+++ controllers/item_controller.rb
@@ -1,9 +1,13 @@
 module ItemController
+
+
   def all_music_albums
+    puts ''
     puts 'All Music Albums'
-    @items.each_with_index do |item, index|
+    puts ''
+    @items.each do |item|
       if item.is_a?(MusicAlbum)
-        print "#{index + 1}. ID: #{item.id} Name: #{item.name} Published on: #{item.publish_date} On Spotify: #{item.on_spotify}"
+        puts "ID: #{item.id} - Name: #{item.name} - Published on: #{item.publish_date} - On Spotify: #{item.on_spotify}"
       end
     end
     puts 'No Music Albums to show' if @items.empty?
