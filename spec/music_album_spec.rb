a1e43602f6cced48638359be2b9ab6a12482fe7b
Lembani Sakala <lembanisakala@gmail.com>
2022-08-04 10:18:22 +0200

    Add can be archived test

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -17,4 +17,8 @@ describe MusicAlbum do
   it 'should return false if music album is not on spotify' do
     expect(@music_album.on_spotify).to eq(false)
   end
+
+  it 'should return false if can be archived method is false' do
+    expect(@music_album.can_be_archived?).to eq(false)
+  end
 end
