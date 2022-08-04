932ed2784f041bb4d0f66ba113b4fe13a67871cf
Lembani Sakala <lembanisakala@gmail.com>
2022-08-04 17:50:45 +0200

    Update genres

--- controllers/genre_controller.rb
+++ controllers/genre_controller.rb
@@ -1,9 +1,28 @@
+require_relative '../models/genre'
+
 module GenreController
+  def load_all_genres
+    genre_store = []
+    genre_file = open('./data/genres.json')
+    if File.exist?(genre_file) && File.read(genre_file) != ''
+      genre_data = genre_file.read
+      JSON.parse(genre_data).each do |genre|
+        genre_store << Genre.new(genre)
+      end
+    else
+      File.write(genre_file, '[]')
+    end
+    genre_store
+  end
+
   def all_genres
+    puts ''
     puts 'All Genres'
-    @genres.each_with_index do |genre, index|
-      puts "#{index + 1}. ID: #{genre.id} Name: #{genre.name}"
+    puts ''
+    @genres.each do |genre|
+      puts "ID: #{genre.id} Name: #{genre.name}"
     end
     puts 'No Genres to show' if @genres.empty?
   end
+
 end
